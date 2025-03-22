9f6def8a39216513b7fd2c49371fe0caa9e40f5f
cleiton016 <cleitonluiz2014@gmail.com>
2025-03-22 14:39:44 -0300

    fix: public router

--- app/routes/public_router.py
+++ app/routes/public_router.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
-from app.controllers.tag_controller import TagController
 
-tag_bp = Blueprint("public", __name__)
+public_bp = Blueprint("public", __name__)
 
-tag_bp.route("/public", methods=["GET"])(lambda : "Hello from public route")
+public_bp.route("/public", methods=["GET"])(lambda : "Hello from public route")
